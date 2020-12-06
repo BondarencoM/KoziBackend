8e539a5511ed5b8c9ba9e563592ba495abe3cfba
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-12-06 20:06:40 +0100

    Added the basics of the auth

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server-express');
+// const {authenticateJWT} = require('./middlewares/jwtAuth');
 const express = require('express');
 const InfluxDataSource = require('./data_sources/InfluxdbDataSource');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers')
 const MongoDataSource = require('./data_sources/mongoDataSource');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const INFLUX_CONFIG = {
   url: process.env.INFLUX_HOST,
@@ -32,6 +35,63 @@ const server = new ApolloServer({
 const app = express();
 
 server.applyMiddleware({ app });
+
+// Example how to use the middleware in speciifc routes
+// app.get('/users', authenticateJWT, (req, res) => {
+//     Do things
+// });
+
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+];
+
+const accessTokenSecret = 'thisshouldbestoredintheenvbutisokayfornow';
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+app.get('/books', authenticateJWT, (req, res) => {
+    res.json(books);
+});
 
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
